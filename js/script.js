898e5b9099b685a5652965db97d61d8f4e50c3d0
Salute-ME <aljihad838@gmail.com>
2025-12-11 00:55:37 +0600

    New Update Released

--- js/script.js
+++ js/script.js
@@ -26,6 +26,26 @@ $('.popularImage').slick({
   autoplay: true,
   autoplaySpeed: 1500,
   dots: true,
+  prevArrow: '<i class="fa-solid fa-circle-arrow-left prevBtn"></i>',
+  nextArrow: '<i class="fa-solid fa-circle-arrow-right nextBtn"></i>', 
+});
+
+// =========================
+
+$('.store').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  dots: true,
+  prevArrow: '<i class="fa-solid fa-circle-arrow-left prevStoreBtn"></i>',
+  nextArrow: '<i class="fa-solid fa-circle-arrow-right nextStoreBtn"></i>', 
+});
+
+// ==================================================
+
+AOS.init({
+    duration:1000,
 });
 
 // ==================================================
